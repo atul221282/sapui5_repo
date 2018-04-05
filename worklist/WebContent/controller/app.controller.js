af5e2832ff327f92dbef46dcd3c06c448b641648
atul221282 <atul221282@gmail.com>
2018-04-05 22:31:50 +0930

    begin

---
+++ worklist/WebContent/controller/app.controller.js
@@ -0,0 +1,8 @@
+//https://sapui5.hana.ondemand.com/#/topic/91f087396f4d1014b6dd926db0e91070
+sap.ui.define([ "sap/ui/core/mvc/Controller" ], function(Controller) {
+	"use strict";
+
+	return Controller.extend("worklist.controller.app", {
+
+	});
+});
